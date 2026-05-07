6d414d61b7407977b2ca5a5c0402629db442b49f
wj <wjkaola123@163.com>
2026-05-07 10:56:31 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    hmr: {
+      clientPort: 80,
+      port: 80,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
 })
